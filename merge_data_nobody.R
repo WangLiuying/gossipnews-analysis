01603277310789dd3415d5b7c97a7a33c3c76b85
WangLiuying <13656036646@163.com>
2017-07-28 02:12:54 +0800

    data merge without body

--- merge_data_nobody.R
+++ merge_data_nobody.R
@@ -10,9 +10,22 @@ account.list <- c("dsmovie .RData","iiiher .RData","yansubagua .RData",
 df.merge <- data.frame()
 for (a in account.list)
 {
+  #formatting
   account <- strsplit(a,split=" .")[[1]][1]
   load(a)
+  df$title <- as.character(df$title)
+  df$time <- as.Date(df$time)
+  df$original <- ifelse(!is.na(df$original),1,0)
+  df$body <- as.character(df$body)
+  read <- as.character(df$read)
+  read[which(str_detect(read,pattern="^\\d{4}$"))]=0
+  read[which(str_detect(read,pattern="^(1|2|3|4|5)\\d{4}$"))]=1
+  read[which(str_detect(read,pattern="^(6|7|8|9)\\d{4}$"))]=2
+  read[which(str_detect(read,pattern="(10万+)|(^\\d{6}$)"))]=3
+  read <- factor(read,levels = c(0,1,2,3),labels=c("[0,10k)","[10k,60k)","[60k,100k)","[100k,infty)"))
+  df$read <- read
   df$account <- account
+  #merge
   df$body <- NULL
   time_tru <- which(df$time=="2016-03-31")
   time_tru <- time_tru[length(time_tru)]
