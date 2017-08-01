4506231c33f2eee44481b5e66e8c39e6777a5d68
WangLiuying <13656036646@163.com>
2017-08-02 01:27:22 +0800

    修改了词语筛选方法：选出文档复现20篇以上的词语

--- topics_extract.R
+++ topics_extract.R
@@ -8,10 +8,10 @@ load("with_body.RData")
 
 title <- df.merge$title
 body <- df.merge$body
-jieba <- worker(user = "./dictionary/topics.txt",
+jieba <- worker(type="mix",user = "./dictionary/topics.txt",
                 stop_word = "./dictionary/chinese_stopword.txt")
 jieba$bylines <- T
-jieba2 <- worker(user = "./dictionary/starname2.txt",
+jieba2 <- worker(user = "./dictionary/topics.txt",
                 stop_word = STOPPATH,byline=T)
 title.seg <- segment(code =unlist(title),jiebar = jieba2)
 body.seg <- segment(code=unlist(body),jiebar=jieba)
@@ -22,11 +22,11 @@ kws <- worker("keywords",user="./dictionary/topics.txt",
 title.key <- vector_keywords(unlist(title.seg),jiebar=kws)
 title.key
 
-kws <- worker("keywords",user="./dictionary/topics.txt",
-              stop_word = "./dictionary/chinese_stopword.txt",topn=50)
-body.seg <- lapply(body.seg,vector_keywords,kws)
+# kws <- worker("keywords",user="./dictionary/topics.txt",
+#               stop_word = "./dictionary/chinese_stopword.txt",topn=100)
+# body.seg <- lapply(body.seg,vector_keywords,kws)
 
-
+rm(df.merge,jieba,jieba2)
 ##wordcloud
 
 library(wordcloud2)
@@ -35,8 +35,8 @@ wordfreq <- data.frame(title.key,
                        Freq=floor(as.numeric(names(title.key))))
 wordcloud2(data=wordfreq,shape="star",size=0.3)
 
-
-
+rm(title,body)
+rm(wordfreq)
 ##topics
 
 library(text2vec)
@@ -45,17 +45,19 @@ help(package="text2vec")
 it <- itoken(iterable = body.seg)
 vocab <- create_vocabulary(it)
 vocab
+vocab <- prune_vocabulary(vocab,term_count_min = 15,doc_proportion_min = 0.0015)
+
 vectorizer <- vocab_vectorizer(vocab)
 corpus <- create_corpus(it,vectorizer)
 dtm <- corpus$get_dtm()
-
+dim(dtm)
 
 
 library(topicmodels)
 
 
-K <- 5:20
-fold <- 5;n <- dim(dtm)[1]
+K <- 5:15
+fold <- 10;n <- dim(dtm)[1]
 alpha=0.1;delta=0.1
 perp <- data.frame()
 for (k in K)
@@ -65,9 +67,10 @@ for (k in K)
   for (f in 1:fold)
   {
     cat("training:k=",k,",fold=",f,"\n")
-    index <- sample(x=1:n,size=n%/%2)
+    index <- sample(x=1:n,size=n%/%4)
+    tindex <- sample(x=setdiff(1:n,index),size=n%/%4)
     corpus.loop <- as.matrix(dtm[index,])
-    testset <- as.matrix(dtm[-index,])
+    testset <- as.matrix(dtm[tindex,])
     gibbs <- LDA(corpus.loop,k,method="Gibbs",
                  control = list(alpha=alpha,delta=delta,burnin=300,iter=500,seed=6646+k+f))
     log.likelihood <- c(log.likelihood,gibbs@loglikelihood)
@@ -75,7 +78,7 @@ for (k in K)
     
   }
   perp <- rbind(perp,data.frame(Num_topics=k,loglikelihood=mean(log.likelihood),perplexity=mean(foldedperp)))
-  save.image("lda_ver2.RData")
+  save.image("lda_ver1.RData")
   Sys.sleep(2)
 }
 
@@ -91,7 +94,7 @@ ggplot(data=perp,mapping=aes(x=Num_topics,y=perplexity))+geom_line()+
 k=12##预计将在10~13左右，待数据完备再标注一次
 alpha=0.1;delta=0.1
 gibbs <- LDA(as.matrix(dtm),k,method="Gibbs",
-             control = list(alpha=alpha,delta=delta,burnin=200,iter=2000,seed=6646))
+             control = list(alpha=alpha,delta=delta,burnin=200,iter=200,seed=6646))
 table(fit.topics <- topics(gibbs))#提出主题
 (topwords <- terms(gibbs,20))#提出主题中最可能的关键词
 
@@ -123,11 +126,11 @@ writeLines(iconv(readLines(paste(dir.path,"/lda.json",sep="")), from = "GBK", to
 
 
 
-# # require:utf-8 encoding
-# lda <- LatentDirichletAllocation$new(15,vocabulary=vocab,doc_topic_prior=0.1,
-#                                      topic_word_prior=0.1)
-# doc_topic_distr <- lda$fit_transform(dtm,n_iter = 2000)
-# topic_word_distr <- lda$get_word_vectors()
-# 
-# lda$plot()
+# require:utf-8 encoding
+lda <- LatentDirichletAllocation$new(12,vocabulary=vocab,doc_topic_prior=0.1,
+                                     topic_word_prior=0.1)
+doc_topic_distr <- lda$fit_transform(dtm,n_iter = 2000)
+topic_word_distr <- lda$get_word_vectors()
+
+lda$plot()
 
